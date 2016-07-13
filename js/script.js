b652bdc77920a37ef558dff2a1b51431a6e33ead
khanh19934 <khanh19934@gmail.com>
2016-07-14 01:23:01 +0700

    Demo

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	 $('#fullpage').fullpage({
+	 	anchors:['firstPage', 'secondPage','thirdPage','fourPage'],
+	 	 sectionsColor : ['#ccc', '#3a3635','white','#d7d7d7'],
+	 	 css3:true,
+	 	 scrollingSpeed:1000
+	 	 
+	 });
+});
